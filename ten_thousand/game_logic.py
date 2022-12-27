fb2496005b47c6e8e6ab748e2d9d8a12dc86d109
Brian Sward <bsward@gmail.com>
2022-12-27 15:42:35 -0800

    dice working

--- ten_thousand/game_logic.py
+++ ten_thousand/game_logic.py
@@ -1,10 +1,16 @@
-#
+# worked with cody delatorre to achieve this code
+import random
 
 
 class GameLogic:
 
-    def roll_dice(self):
-        pass
+    def roll_dice(n):
+        """method which will return you n random integers between 1 - 6"""
+        roll = []
+        for x in range(n):
+            roll.append(random.randint(1, 6))
+        roll = tuple(roll)
+        return roll
 
     def calculate_score(self):
         pass
